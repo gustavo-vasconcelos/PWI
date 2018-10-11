abb2370ce98068df7a33ae2caeb0d09b28e81690
Gustavo Vasconcelos <gustavofamoso48@gmail.com>
2018-10-11 18:53:48 +0100

    changes

--- F04/F04EX01/js/vue-instance.js
+++ F04/F04EX01/js/vue-instance.js
@@ -3,19 +3,38 @@ const vm = new Vue({
     data: {
         msg: "A ESMAD é uma escola do IPP",
         styles: {
-            "fontSize": "12pt",
-            "color": "black",
-            "display": "inline"
+            fontSize: "12pt",
+            color: "black",
+            backgroundColor: "auto",
+            display: "inline"
         },
     },
     methods: {
         changeColor: function (color) {
             this.styles.color = color
         },
+        toggleColor: function () {
+            if (this.styles.color === "black") {
+                this.changeColor("red")
+            } else {
+                this.changeColor("black")
+            }
+            //this.styles.color = (this.styles.color === "black") ? this.changeColor("red") : this.changeColor("black")
+        },
+        changeBackgroundColor: function (color) {
+            this.styles.backgroundColor = color
+        },
+        toggleBackgroundColor: function () {
+            if (this.styles.backgroundColor === "transparent") {
+                this.changeBackgroundColor("yellow")
+            } else {
+                this.changeBackgroundColor("transparent")
+            }
+        },
         changeFontSize: function () {
             this.styles.fontSize = (this.styles.fontSize === "12pt") ? this.styles.fontSize = "30pt" : this.styles.fontSize = "12pt"
         },
-        toggleVisibility: function() {
+        toggleVisibility: function () {
             this.styles.display = (this.styles.display === "inline") ? this.styles.display = "none" : this.styles.display = "inline"
         }
     }
---
+++ F04/F04EX03/js/vue-instance.js
@@ -0,0 +1,8 @@
+const vm = new Vue({
+    el: "#intro",
+    data: {
+        teams: [
+            {name: "benfica", year: "1910"}
+        ]
+    }
+})
