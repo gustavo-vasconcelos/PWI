f7aaaaacf34212b635f2ab8851aad61b9eaf98dd
Gustavo Vasconcelos <9170196@esmad.ipp.pt>
2018-10-11 20:46:52 +0100

    changes

--- F04/F04EX03/js/vue-instance.js
+++ F04/F04EX03/js/vue-instance.js
@@ -2,9 +2,29 @@ const vm = new Vue({
     el: "#intro",
     data: {
         teams: [
-            { name: "porto", year: "1912" },
-            { name: "sporting", year: "1914" },
-            { name: "benfica", year: "1910" }
+            { name: "Porto", year: "1912", location: "Porto" },
+            { name: "Sporting", year: "1914", location: "Lisboa" },
+            { name: "Benfica", year: "1910", location: "Lisboa" }
         ]
+    },
+    methods: {
+        addTeam: function () {
+            let teamName = prompt("Indique o nome da equipa:")
+            let teamYear = prompt(`Indique o ano de fundação da equipa ${teamName}:`)
+            let teamLocation = prompt(`Indique o local de origem da equipa ${teamName}:`)
+            this.teams.push({ name: teamName, year: teamYear, location: teamLocation })
+        },
+        filterByLocation: function () {
+            let availableLocations = []
+            this.teams.filter(teamData => {
+                if (availableLocations.indexOf(teamData.location) === -1) {
+                    availableLocations.push(teamData.location)
+                }
+            })
+            let location = prompt(`Escolha um dos locais de origem disponíveis (${availableLocations.join(", ")}):`)
+            this.teams = this.teams.filter(teamData => {
+                return teamData.location === location
+            })
+        }
     }
 })
