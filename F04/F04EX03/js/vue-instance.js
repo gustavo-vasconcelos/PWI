a0583e34f65fbbd5b1504aa315449e151d3bdcaf
Gustavo Vasconcelos <9170196@esmad.ipp.pt>
2018-10-17 23:14:04 +0100

    changes

--- F04/F04EX03/js/vue-instance.js
+++ F04/F04EX03/js/vue-instance.js
@@ -5,26 +5,43 @@ const vm = new Vue({
             { name: "Porto", year: "1912", location: "Porto" },
             { name: "Sporting", year: "1914", location: "Lisboa" },
             { name: "Benfica", year: "1910", location: "Lisboa" }
-        ]
+        ],
+        filteringLocation: "",
+        styles: {
+            btn: ["btn", "btn-primary"]
+        }
+        
     },
     methods: {
-        addTeam: function () {
+        addTeam() {
             let teamName = prompt("Indique o nome da equipa:")
             let teamYear = prompt(`Indique o ano de fundação da equipa ${teamName}:`)
             let teamLocation = prompt(`Indique o local de origem da equipa ${teamName}:`)
             this.teams.push({ name: teamName, year: teamYear, location: teamLocation })
         },
-        filterByLocation: function () {
-            let availableLocations = []
-            this.teams.filter(teamData => {
-                if (availableLocations.indexOf(teamData.location) === -1) {
-                    availableLocations.push(teamData.location)
-                }
-            })
-            let location = prompt(`Escolha um dos locais de origem disponíveis (${availableLocations.join(", ")}):`)
-            this.teams = this.teams.filter(teamData => {
-                return teamData.location === location
-            })
+        filterByLocation() {
+            if (!this.filteringLocation) {
+                let availableLocations = []
+                this.teams.filter(teamData => {
+                    if (availableLocations.indexOf(teamData.location) === -1) {
+                        availableLocations.push(teamData.location)
+                    }
+                })
+                this.filteringLocation = prompt(`Escolha um dos locais de origem disponíveis (${availableLocations.join(", ")}):`)
+            } else {
+                this.filteringLocation = ""
+            }
+        }
+    },
+    computed: {
+        printTeams() {
+            if (!this.filteringLocation) {
+                return this.teams
+            } else {
+                return this.teams.filter(team => {
+                    return team.location.toUpperCase() === this.filteringLocation.toUpperCase()
+                })
+            }
         }
     }
 })
