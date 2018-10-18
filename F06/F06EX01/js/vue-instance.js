c0528505eafc14f4d80fd40ef5b645ebaa7aac26
Gustavo Vasconcelos <9170196@esmad.ipp.pt>
2018-10-18 16:07:05 +0100

    changes

---
+++ F06/F06EX01/js/vue-instance.js
@@ -0,0 +1,60 @@
+const vm = new Vue({
+    el: "#intro",
+    data: {
+        tasks: [],
+        form: {
+            taskName: "",
+            taskType: "personal"
+        },
+        filterTaskType: ""
+    },
+    methods: {
+        addTask() {
+            this.tasks.push({
+                name: this.form.taskName,
+                type: this.form.taskType
+            })
+        },
+        removeTask(index) {
+            swal({
+                title: "Deseja mesmo remover?",
+                text: `A tarefa ${this.tasks[index].name} será removida para sempre!`,
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+            }).then((willDelete) => {
+                if (willDelete) {
+                    this.tasks.splice(index, 1)
+                    swal("Tarefa removida.", {
+                        icon: "success",
+                    })
+                }
+            })
+        },
+        validateSubmit() {
+            let sameName = this.tasks.some((task) => {
+                return task.name.toLowerCase() == this.form.taskName.toLowerCase()
+            })
+            if (!this.form.taskName) {
+                swal("Erro", "Campo nome obrigatório.", "error")
+            } else if (sameName) {
+                swal("Erro", "Já existe uma tarefa com o mesmo nome.", "error")
+            } else {
+                this.addTask()
+                this.form.taskName = ""
+                this.form.taskType = "personal"
+            }
+        }
+    },
+    computed: {
+        printTasks() {
+            if (!this.filterTaskType) {
+                return this.tasks
+            } else {
+                return this.tasks.filter(task => {
+                    return task.type === this.filterTaskType
+                })
+            }
+        }
+    }
+})
