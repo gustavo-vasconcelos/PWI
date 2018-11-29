ff8a1b10092e71a47b9bee32d4496b07e57c0adc
Gustavo Vasconcelos <9170196@esmad.ipp.pt>
2018-11-29 02:49:54 +0000

    changes

--- F11/f11ex03/src/App.vue
+++ F11/f11ex03/src/App.vue
@@ -12,34 +12,80 @@ export default {
   data() {
     return {
       actors: [
+        /*
         {
           id: 1,
+          /*
           name: "Bruce Willis",
           nationality: "Germany",
           dateBirth: "1955-03-19",
           photo:
             "https://pmcvariety.files.wordpress.com/2018/04/bruce-willis.jpg?w=1000",
-          imdb: "nm0000246"
+          imdb: "nm0000246",
+          
+          tmdb: 62
         },
         {
           id: 2,
+          /*
           name: "Leonardo DiCaprio",
           nationality: "United States",
           dateBirth: "1974-11-11",
           photo:
             "https://assets3.thrillist.com/v1/image/1656352/size/tmg-article_default_mobile.jpg",
-          imdb: "nm0000138"
+          imdb: "nm0000138",
+          
+          tmdb: 6193
         },
         {
           id: 3,
+          /*
           name: "Brad Pitt",
           nationality: "United States",
           dateBirth: "1963-12-18",
           photo:
             "https://postmediacanoe.files.wordpress.com/2018/11/brad-pitt.jpg",
-          imdb: "nm0000093"
+          imdb: "nm0000093",
+          
+          tmdb: 287
         }
-      ]
+        */
+      ],
+      movies: [
+        {
+          id: 1,
+          trailerId: "kg_jH47u480",
+          imdb: "tt0120591"
+        },
+        {
+          id: 2,
+          trailerId: "VG9AGf66tXM",
+          imdb: "tt0167404"
+        },
+        {
+          id: 3,
+          trailerId: "8KtYRALe-xo",
+          imdb: "tt1320253"
+        },
+        {
+          id: 4,
+          trailerId: "ip_CYHdyUBs",
+          imdb: "tt1764651"
+        },
+        {
+          id: 5,
+          trailerId: "-iRajLSA8TA",
+          imdb: "tt0120338"
+        }
+      ],
+      movieActor: [
+        { movieId: 1, actorId: 1 },
+        { movieId: 2, actorId: 1 },
+        { movieId: 3, actorId: 1 },
+        { movieId: 4, actorId: 1 },
+        { movieId: 5, actorId: 2 }
+      ],
+      mostPopularActors: []
     };
   },
   methods: {
@@ -51,7 +97,47 @@ export default {
     },
     getIndexById(id) {
       return this.actors.findIndex(actor => actor.id === id);
+    },
+    getMostPopularActors() {
+      let jsonData = [];
+      //1-20
+      fetch(
+        "https://api.themoviedb.org/3/person/popular?api_key=30a9bd883b54f8b08a0247941e2bbec5&language=pt-PT&page=1"
+      )
+        .then(response => response.json())
+        .then(myJson => myJson.results.forEach(actor => jsonData.push(actor)));
+      //21-40
+      fetch(
+        "https://api.themoviedb.org/3/person/popular?api_key=30a9bd883b54f8b08a0247941e2bbec5&language=pt-PT&page=2"
+      )
+        .then(response => response.json())
+        .then(myJson => myJson.results.forEach(actor => jsonData.push(actor)));
+  
+      jsonData.forEach((actor, index) => {
+        console.log(actor)
+        this.actors.push({ id: index, tmdb: actor.id, name: actor.name });
+      });
+
+      return jsonData;
+    },
+    processMostPopularActors() {
+      this.mostPopularActors.forEach((actor, index) => {
+        console.log(actor)
+        this.actors.push({ id: index, tmdb: actor.id, name: actor.name });
+      });
     }
+  },
+  created() {
+    if (!localStorage.mostPopularActors) {
+      this.mostPopularActors = this.getMostPopularActors();
+      this.processMostPopularActors();
+    }
+    if (!localStorage.actors) {
+    }
+    /*
+    if (!localStorage.movies) localStorage.movies = JSON.stringify(this.movies);
+    if (!localStorage.movieActor)
+      localStorage.movieActor = JSON.stringify(this.movieActor);*/
   }
 };
 </script>
