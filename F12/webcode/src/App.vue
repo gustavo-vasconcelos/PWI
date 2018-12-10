aedaf4bc3dd6239daea88dac386b56e609941ae8
Gustavo Vasconcelos <9170196@esmad.ipp.pt>
2018-12-10 23:17:36 +0000

    changes

---
+++ F12/webcode/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+	<div id="app" class="container">
+		<div class="jumbotron px-0 pt-0">
+			<Navbar/>
+			<router-view/>
+		</div>
+	</div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue"
+export default {
+	components: {
+		Navbar
+	}
+}
+</script>
